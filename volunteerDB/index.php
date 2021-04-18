2c5c3ce004e10aef18b32c0bbfede67482b6a2f6
Tara Ram Mohan <rammohant@vcu.edu>
2021-04-18 05:40:52 -0400

    Add navbar

--- volunteerDB/index.php
+++ volunteerDB/index.php
@@ -8,13 +8,26 @@
 
 <body>
 
-<div class="container-fluid mt-3 mb-3">
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Volunter DB</a>
+    </div>
+    <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+    	<li><a href="volunteer_events.php">Volunteer Events</a></li>
+    	<li><a href="organization.php">Organizations</a></li>
+    	<li><a href="signup.php">Sign up for event</a></li>
+    </ul>
+  </div>
+</nav>
+
+<!-- <div class="container-fluid mt-3 mb-3">
     <ul>
     	<li><a href="volunteer_events.php">List all volunteer events</a></li>
 		<li><a href="organization.php">List all organizations</a></li>
 		<li><a href="signup.php">Sign up for event</a></li>
     </ul>
-</div>
-
+</div> -->
 </body>
 </html>
