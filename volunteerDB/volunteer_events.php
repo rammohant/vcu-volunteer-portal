0e1f79590cad0200c1044b4f56302a5e73afcaf4
Tara Ram Mohan <rammohant@vcu.edu>
2021-04-19 09:27:20 -0400

    Fix volunteer opportunity table not populating error

--- volunteerDB/volunteer_events.php
+++ volunteerDB/volunteer_events.php
@@ -13,19 +13,22 @@
 <div class="container-fluid mt-3 mb-3">
 	<h4>Volunteer Opportunities</h4>
 	<div class="table-responsive">
-		<table id="table-employee" class="table table-bordered table-striped">
+		<table id="table-volunteer-opportunities" class="table table-bordered table-striped">
 			<thead>
 				<tr>
+					<th>ID</th>
 					<th>Title</th>
-					<th>Name</th>
-					<th>Salary</th>
-					<th>Manager</th>
-					<th>Department</th>
+					<th>Description</th>
+					<th>Link</th>
+					<th>Type</th>
+					<th>Date</th>
+					<th>Available Spots</th>
+					<th>Skills Needed</th>
+					<th>Age Minimum</th>
 				</tr>
 			</thead>
 		</table>
 	</div>
 </div>
-
 </body>
 </html>
