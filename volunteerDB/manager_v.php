e69b14f50c9fac2c12343c538354b45f144e626d
Tara Ram Mohan <rammohant@vcu.edu>
2021-04-24 15:21:46 -0400

    Make separate manager connection file

--- volunteerDB/manager_v.php
+++ volunteerDB/manager_v.php
@@ -1,43 +1,6 @@
 <html>
 <head>
 <title>HR database - Organizer/Admin View</title>
-<!-- check if user is logged in and a manager -->
-<?php 
-if (!isset($_SESSION['userID']))
-{
-    // If the page is receiving the email and password from the login form then verify the login data
-    if (isset($_POST['email']) && isset($_POST['password']))
-    {
-        $stmt = $conn->prepare("SELECT userID, password FROM users WHERE email=:email and type IN ('manager','admin')");
-        $stmt->bindValue(':email', $_POST['email']);
-        $stmt->execute();
-        
-        $queryResult = $stmt->fetch();
-        
-        // Verify password submitted by the user with the hash stored in the database
-        if(!empty($queryResult) && password_verify($_POST["password"], $queryResult['password']))
-        {
-            // Create session variable
-            $_SESSION['userID'] = $queryResult['userID'];
-            
-            // Redirect to URL
-            header("Location: http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
-        } else {
-            // Password mismatch
-            require('login.php');
-            echo("Please login to your manager account to access this page.");
-            exit();
-        }
-    }
-    else
-    {
-        // Show login page
-        require('login.php');
-        echo("Please login to your manager account to access this page.");
-        exit();
-    }
-}
-?>
 
 <?php require_once('header.php'); ?>
 
@@ -59,10 +22,24 @@ if (!isset($_SESSION['userID']))
 
 </head>
 
-
-<?php require_once('connection.php'); ?>
+<!-- check if user is logged in and a manager -->
+<?php require_once('manager_connection.php'); ?>
 
 <body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Volunter DB</a>
+    </div>
+    <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+    	<li><a href="volunteer_opportunities.php">Volunteer Events</a></li>
+    	<li><a href="addEvent.php">Add events</a></li>
+    	<li><a href="manager_v.php">Manager Portal</a></li>
+    </ul>
+  </div>
+</nav>
 
 <div class="container-fluid mt-3 mb-3">
 	<h4>Volunteer Opportunities</h4>
