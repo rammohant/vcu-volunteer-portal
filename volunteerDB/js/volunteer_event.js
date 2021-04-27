5226bb2087ad01815d7e931c678d6c91393bae65
Tara Ram Mohan <rammohant@vcu.edu>
2021-04-26 22:25:43 -0400

    Same file name change

--- volunteerDB/js/volunteer_event.js
+++ volunteerDB/js/volunteer_event.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 		"searching": false,
 		"paging": false,
 		"ajax":{
-			url:"volunteer_events-action.php",
+			url:"volunteer_event-action.php",
 			type:"POST",
 			data:{
 					action:'listVolunteerEvent'
