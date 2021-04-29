4ceb36c6161a5edb34bbca30c57e68e3365cc44a
Tara Ram Mohan <rammohant@vcu.edu>
2021-04-29 09:25:30 -0400

    Added manager view only table

--- volunteerDB/manager_v.php
+++ volunteerDB/manager_v.php
@@ -11,7 +11,7 @@
 <style type="text/css">
 h2 {
     text-align: center;
-    font-size: 30px; 
+    font-size: 25px; 
     padding-top: 25px; 
     font-family: "Verdana";
     font-weight: bold; 
@@ -62,17 +62,6 @@ li a:hover {
     background-color: #111;
 }
 
-table {
-  width: 100%; 
-  background-color: #615F5F;
-  opacity: 0.80;
-}
-
-tr{
-    color: #EEEAE9;
-    font-family: "Verdana";
-}
-
 
 </style>
 <?php require_once('header.php'); ?>
@@ -92,10 +81,58 @@ tr{
 	<li><a href="signup.php">Sign up</a></li>
 </ul>
 
+<?php 
+
+global $conn;
+$this->userID = $_SESSION['userID']=$user->id;
+
+$sql = "SELECT v.eventID, v.Title, v.description, v.link, v.type, v.DateRange, v.available_spots, v.needed_skills, v.age_minimum,  v.approver
+                     FROM v_volunteer_ops v where organizer=$id";
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo "<h2>Welcome to the Manager Portal</h2>";
+        echo "<p>View, add, and delete volunteer events for your organization below.</p>";
+        echo "<table class='table table-dark table-stripped' style='width:80%; margin-left: 10%; margin-right: 10%; opacity: 90%'>";
+        echo "<tr>";
+        echo "<th>Title</th>";
+        echo "<th>Description</th>";
+        echo "<th>Link</th>";
+        echo "<th>Type</th>";
+        echo "<th>Date</th>";
+        echo "<th>Available Spots</th>";
+        echo "<th>Skills Needed</th>";
+        echo "<th>Age Minimum</th>";
+        echo "<th>Approver</th>";
+        
+        echo "</tr>";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+            echo "<td>" . $row['Title'] . "</td>";
+            echo "<td>" . $row['description'] . "</td>";
+            echo "<td>" . $row['link'] . "</td>";
+            echo "<td>" . $row['type'] . "</td>";
+            echo "<td>" . $row['DateRange'] . "</td>";
+            echo "<td>" . $row['available_spots'] . "</td>";
+            echo "<td>" . $row['needed_skills'] . "</td>";
+            echo "<td>" . $row['age_minimum'] . "</td>";
+            echo "<td>" . $row['approver'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+?>
+
 <div class="container-fluid mt-3 mb-3">
 	<h2>Welcome to the Manager Portal</h2>
 	<p>View, add, and delete volunteer events for your organization below.</p>
-	
+
 	<div class="pb-3">
 		<button type="button" id="addEvent" class="btn btn-primary btn-sm">Add Event</button>
 	</div> 
