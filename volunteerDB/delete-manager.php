82b1d7569e64787e584235a1a53d2958e46a420c
Tara Ram Mohan <rammohant@vcu.edu>
2021-05-05 20:43:15 -0400

    Fix delete from manager view not working

--- volunteerDB/delete-manager.php
+++ volunteerDB/delete-manager.php
@@ -2,45 +2,35 @@
 
 require_once('connection.php'); // Using database connection file here
 
-global $conn;
+// global $conn;
 
-if(isset($_POST['Title'])) {
+// if(isset($_POST['Title'])) {
 
-    $title = $_POST['Title'];
+//     $title = $_POST['Title'];
 
-    $stmt = $mysqli->prepare("DELETE FROM volunteer_events WHERE title = ?");
-    $stmt->bind_param('title', $title);
-    $stmt->execute(); 
-    $stmt->close();
-    echo "Record deleted"; // display error message if not delete
-    header("location:index.php"); // redirects to all records page
-    exit;	
-}
-
-// $id = $_POST['eventID']; // get id through query string
-
-// $del = mysqli_query($link,"DELETE FROM volunteer_events WHERE eventID = '$id'"); // delete query
-
-// if ($_POST['ID']) {
-            
-//     $sqlQuery = "DELETE FROM volunteer_events WHERE eventID = ':ID'";
-    
-//     $stmt = $conn->prepare($sqlQuery);
-//     $stmt->bindValue(':ID', $_POST["eventID"]);
-//     $stmt->execute();
-
-// }
-
-// if($del)
-// {
-//     mysqli_close($link); // Close connection
+//     $stmt = $mysqli->prepare("DELETE FROM volunteer_events WHERE title = ?");
+//     $stmt->bind_param('title', $title);
+//     $stmt->execute(); 
+//     $stmt->close();
 //     echo "Record deleted"; // display error message if not delete
-//     header("location:index.php"); // redirects to all records page
+//     header("location:manager_v.php"); // redirects to all records page
 //     exit;	
 // }
 
-// else
-// {
-//     echo "Error deleting record"; // display error message if not delete
-// }
+$title = $_POST['Title']; // get id through query string
+
+$del = mysqli_query($link,"DELETE FROM volunteer_events WHERE title = '$title'"); // delete query
+
+if($del)
+{
+    mysqli_close($link); // Close connection
+    echo "Record deleted"; // display error message if not delete
+    header("location:manager_v.php"); // redirects to all records page
+    exit;	
+}
+
+else
+{
+    echo "Error deleting record"; // display error message if not delete
+}
 ?>
--- volunteerDB/volunteer_v.php
+++ volunteerDB/volunteer_v.php
@@ -83,64 +83,10 @@ tr{
 
 <?php 
 
-global $conn;
-$volunteerID = $_SESSION['userID']; 
+// global $conn;
+// $volunteerID = $_SESSION['userID']; 
 
-$stmt = $conn->prepare("SELECT v.eventID, 
-v.title as 'Title', 
-v.description as 'Description', 
-v.link as 'Link', 
-v.type as 'Type', 
-v.DateRange as 'Date', 
-v.available_spots as 'Available Spots',
-v.needed_skills as 'Skills Needed',
-v.age_minimum as 'Age Minimum',
-v.organization as 'Organization', 
-v.number as 'Contact Number', 
-v.email as 'Contact Email'
-FROM v_all_volunteer_signups v where volunteerID = ?");
-
-$stmt->bind_param('volunteerID', $_SESSION['userID']);
-$stmt->execute();
-
-echo "<h2>Welcome to the Manager Portal</h2>";
-        echo "<p>View, add, and delete volunteer events for your organization below.</p>";
-        echo "<table class='table table-dark table-stripped' style='width:80%; margin-left: 10%; margin-right: 10%; opacity: 90%'>";
-        echo "<tr>";
-        echo "<th>ID</th>";
-        echo "<th>Title</th>";
-        echo "<th>Description</th>";
-        echo "<th>Organization</th>";
-        echo "<th>Email</th>";
-        echo "<th>Link</th>";
-        echo "<th>Type</th>";
-        echo "<th>Date</th>";
-        echo "<th>Available Spots</th>";
-        echo "<th>Skills Needed</th>";
-        echo "<th>Age Minimum</th>";
-        echo "</tr>";
-
-while ($row = $stmt->fetch()) {
-        echo "<tr>";
-        echo "<td>" . $row['eventID'] . "</td>";
-        echo "<td>" . $row['Title'] . "</td>";
-        echo "<td>" . $row['Description'] . "</td>";
-        echo "<td>" . $row['Organization'] . "</td>";
-        echo "<td>" . $row['Contact Email'] . "</td>";
-        echo "<td>" . $row['Link'] . "</td>";
-        echo "<td>" . $row['Type'] . "</td>";
-        echo "<td>" . $row['Date'] . "</td>";
-        echo "<td>" . $row['Available Spots'] . "</td>";
-        echo "<td>" . $row['Skills Needed'] . "</td>";
-        echo "<td>" . $row['Age Minimum'] . "</td>";
-        echo "<td><form action='delete-user.php' method='POST'><input type='hidden' name='eventID' value='".$row['eventID']."'/><input type='submit' name='submit-btn' value='Delete' /></form></td></tr>";
-        echo "</tr>";
-    }
-        echo "</table>";
-        mysqli_free_result($result);
-
-
- // $sql = "SELECT v.eventID, 
+// $stmt = $conn->prepare("SELECT v.eventID, 
 // v.title as 'Title', 
 // v.description as 'Description', 
 // v.link as 'Link', 
@@ -152,11 +98,12 @@ while ($row = $stmt->fetch()) {
 // v.organization as 'Organization', 
 // v.number as 'Contact Number', 
 // v.email as 'Contact Email'
-// FROM v_all_volunteer_signups v where v.volunteerID = '$volunteerID'";
+// FROM v_all_volunteer_signups v where volunteerID = ?");
 
-// if($result = mysqli_query($link, $sql)){
-//     if(mysqli_num_rows($result) > 0){
-//         echo "<h2>Welcome to the Manager Portal</h2>";
+// $stmt->bind_param('volunteerID', $_SESSION['userID']);
+// $stmt->execute();
+
+// echo "<h2>Welcome to the Manager Portal</h2>";
 //         echo "<p>View, add, and delete volunteer events for your organization below.</p>";
 //         echo "<table class='table table-dark table-stripped' style='width:80%; margin-left: 10%; margin-right: 10%; opacity: 90%'>";
 //         echo "<tr>";
@@ -172,30 +119,83 @@ while ($row = $stmt->fetch()) {
 //         echo "<th>Skills Needed</th>";
 //         echo "<th>Age Minimum</th>";
 //         echo "</tr>";
-//         while($row = mysqli_fetch_array($result)){
-//             echo "<tr>";
-//             echo "<td>" . $row['eventID'] . "</td>";
-//             echo "<td>" . $row['Title'] . "</td>";
-//             echo "<td>" . $row['Description'] . "</td>";
-//             echo "<td>" . $row['Organization'] . "</td>";
-//             echo "<td>" . $row['Contact Email'] . "</td>";
-//             echo "<td>" . $row['Link'] . "</td>";
-//             echo "<td>" . $row['Type'] . "</td>";
-//             echo "<td>" . $row['Date'] . "</td>";
-//             echo "<td>" . $row['Available Spots'] . "</td>";
-//             echo "<td>" . $row['Skills Needed'] . "</td>";
-//             echo "<td>" . $row['Age Minimum'] . "</td>";
-//             echo "<td><form action='delete-user.php' method='POST'><input type='hidden' name='eventID' value='".$row['eventID']."'/><input type='submit' name='submit-btn' value='Delete' /></form></td></tr>";
-//             echo "</tr>";
-//         }
+
+// while ($row = $stmt->fetch()) {
+//         echo "<tr>";
+//         echo "<td>" . $row['eventID'] . "</td>";
+//         echo "<td>" . $row['Title'] . "</td>";
+//         echo "<td>" . $row['Description'] . "</td>";
+//         echo "<td>" . $row['Organization'] . "</td>";
+//         echo "<td>" . $row['Contact Email'] . "</td>";
+//         echo "<td>" . $row['Link'] . "</td>";
+//         echo "<td>" . $row['Type'] . "</td>";
+//         echo "<td>" . $row['Date'] . "</td>";
+//         echo "<td>" . $row['Available Spots'] . "</td>";
+//         echo "<td>" . $row['Skills Needed'] . "</td>";
+//         echo "<td>" . $row['Age Minimum'] . "</td>";
+//         echo "<td><form action='delete-user.php' method='POST'><input type='hidden' name='eventID' value='".$row['eventID']."'/><input type='submit' name='submit-btn' value='Delete' /></form></td></tr>";
+//         echo "</tr>";
+//     }
 //         echo "</table>";
 //         mysqli_free_result($result);
-//     } else{
-//         echo "No records matching your query were found.";
-//     }
-// } else{
-//     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-// }
+
+
+$sql = "SELECT v.eventID, 
+v.title as 'Title', 
+v.description as 'Description', 
+v.link as 'Link', 
+v.type as 'Type', 
+v.DateRange as 'Date', 
+v.available_spots as 'Available Spots',
+v.needed_skills as 'Skills Needed',
+v.age_minimum as 'Age Minimum',
+v.organization as 'Organization', 
+v.number as 'Contact Number', 
+v.email as 'Contact Email'
+FROM v_all_volunteer_signups v where v.volunteerID = '$volunteerID'";
+
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo "<h2>Welcome to the Manager Portal</h2>";
+        echo "<p>View, add, and delete volunteer events for your organization below.</p>";
+        echo "<table class='table table-dark table-stripped' style='width:80%; margin-left: 10%; margin-right: 10%; opacity: 90%'>";
+        echo "<tr>";
+        echo "<th>ID</th>";
+        echo "<th>Title</th>";
+        echo "<th>Description</th>";
+        echo "<th>Organization</th>";
+        echo "<th>Email</th>";
+        echo "<th>Link</th>";
+        echo "<th>Type</th>";
+        echo "<th>Date</th>";
+        echo "<th>Available Spots</th>";
+        echo "<th>Skills Needed</th>";
+        echo "<th>Age Minimum</th>";
+        echo "</tr>";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+            echo "<td>" . $row['eventID'] . "</td>";
+            echo "<td>" . $row['Title'] . "</td>";
+            echo "<td>" . $row['Description'] . "</td>";
+            echo "<td>" . $row['Organization'] . "</td>";
+            echo "<td>" . $row['Contact Email'] . "</td>";
+            echo "<td>" . $row['Link'] . "</td>";
+            echo "<td>" . $row['Type'] . "</td>";
+            echo "<td>" . $row['Date'] . "</td>";
+            echo "<td>" . $row['Available Spots'] . "</td>";
+            echo "<td>" . $row['Skills Needed'] . "</td>";
+            echo "<td>" . $row['Age Minimum'] . "</td>";
+            echo "<td><form action='delete-user.php' method='POST'><input type='hidden' name='eventID' value='".$row['eventID']."'/><input type='submit' name='submit-btn' value='Delete' /></form></td></tr>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
 ?>
 
 </body>
