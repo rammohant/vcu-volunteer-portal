4dd2eedccbf30c1a317b884eac924ca42f594a22
Tara Ram Mohan <rammohant@vcu.edu>
2021-05-08 00:28:20 -0400

    Update connection-manager.php

--- volunteerDB/connection-manager.php
+++ volunteerDB/connection-manager.php
@@ -19,53 +19,39 @@ try {
 }
 
 // Start or resume session variables
-//session_start();
+session_start();
 
 // If the user_ID session is not set, then the user has not logged in yet
 if (!isset($_SESSION['userID']))
 {
     // If the page is receiving the email and password from the login form then verify the login data
-    if (isset($_POST['email']) && isset($_POST['password'])) {
-        $stmt = $conn->prepare("SELECT userID, password FROM users WHERE email=:email and type IN ('organizer','admin')");
+    if (isset($_POST['email']) && isset($_POST['password']))
+    {
+        $stmt = $conn->prepare("SELECT userID, password FROM users WHERE email=:email and type like 'manager'");
         $stmt->bindValue(':email', $_POST['email']);
         $stmt->execute();
         
         $queryResult = $stmt->fetch();
-
-        $stmt2 = $conn->prepare("SELECT userID, password FROM users WHERE email=:email and type IN ('volunteer')");
-        $stmt2->bindValue(':email', $_POST['email']);
-        $stmt2->execute();
-        
-        $queryResult2 = $stmt->fetch();
         
         // Verify password submitted by the user with the hash stored in the database
         if(!empty($queryResult) && password_verify($_POST["password"], $queryResult['password']))
         {
             // Create session variable
             $_SESSION['userID'] = $queryResult['userID'];
+            $username = $_SESSION['userID'];
             
-            // Redirect to URL
+            // Redirect to URL 
             header("Location: http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
-
-           // header("location:manager_v.php"); 
-        // } else if(!empty($queryResult) && password_verify($_POST["password"], $queryResult2['password'])) {
-        //     // Create session variable
-        //     $_SESSION['userID'] = $queryResult['userID'];
-        //     // Redirect to URL
-        //     header("location:volunteer_v.php"); 
         } else {
             // Password mismatch
-            // header("location:index.php"); 
-            echo("Requires manager account to access.");
-            require('login.php');
+            require('manager-login.php');
             exit();
         }
-    } 
+    }
     else
     {
         // Show login page
-        echo("Requires manager account to access.");
-        require('login.php');
+        require('manager-login.php');
         exit();
     }
 }
