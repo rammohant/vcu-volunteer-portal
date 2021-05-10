14d990f2f804100f469493a7347d6a02d0adc5be
Tara Ram Mohan <rammohant@vcu.edu>
2021-05-09 20:19:31 -0400

    Update register.php

--- volunteerDB/register.php
+++ volunteerDB/register.php
@@ -3,17 +3,19 @@
 require_once "config.php";
 
 // Define variables and initialize with empty values
-$email = $password = $first_name = $last_name = $confirm_password =  $university = $languages = $skills = $vaccinated = "";
+$email = $password = $first_name = $last_name = $confirm_password = $university = $languages = $skills = $vaccinated = "";
 $email_err = $password_err = $firstname_err = $lastname_err = $confirm_password_err = "";
 
 // Processing form data when form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     // Validate first_name
     if (empty(trim($_POST["first_name"]))) {
         $firstname_err = "Please enter your first name.";
     } else {
         $first_name = trim($_POST["first_name"]);
     }
+
     // Validate last_name
     if (empty(trim($_POST["last_name"]))) {
         $lastname_err = "Please enter your last name.";
@@ -40,15 +42,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         
 //         // Prepare a select statement
 //         $sql = "SELECT id FROM users WHERE email = ?";
+
 //         if ($stmt = mysqli_prepare($link, $sql)) {
 //             // Bind variables to the prepared statement as parameters
 //             mysqli_stmt_bind_param($stmt, "s", $param_email);
+
 //             // Set parameters
 //             $param_email = trim($_POST["email"]);
+
 //             // Attempt to execute the prepared statement
 //             if (mysqli_stmt_execute($stmt)) {
 //                 /* store result */
 //                 mysqli_stmt_store_result($stmt);
+
 //                 if (mysqli_stmt_num_rows($stmt) == 1) {
 //                     $email_err = "This email is already taken.";
 //                 } else {
@@ -57,10 +63,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 //             } else {
 //                 echo "Oops! Something went wrong. Please try again later.";
 //             }
+
 //             // Close statement
 //             mysqli_stmt_close($stmt);
 //         }
     }
+
     // Validate password
     if (empty(trim($_POST["password"]))) {
         $password_err = "Please enter a password.";
@@ -69,6 +77,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $password = trim($_POST["password"]);
     }
+
     // Validate confirm password
     if (empty(trim($_POST["confirm_password"]))) {
         $confirm_password_err = "Please confirm password.";
@@ -78,36 +87,55 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $confirm_password_err = "Password did not match.";
         }
     }
+
     // Check input errors before inserting in database
     if (empty($email_err) && empty($password_err) && empty($confirm_password_err && empty($firstname_err) && empty($lastname_err))) {
+
         // Prepare an insert statement
         $sql = "INSERT INTO users (email, password, first_name, last_name, type) VALUES (?, ?, ?, ?, ?)";
+        $sql_university = "INSERT INTO university (university_name) VALUES (?)";
+        $sql_volunteer = "INSERT INTO volunteers (userID, university_name) VALUES (?, ?)";
+
+        try {
         if ($stmt = mysqli_prepare($link, $sql)) {
+        // if ($stmt = mysqli_prepare($link, $sql) && $stmt_university = mysqli_prepare($link, $sql_university) && $stmt_volunteer = mysqli_prepare($link, $sql_volunteer)) {
+
             // Set parameters
             $param_email = $email;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
             $param_firstname = $first_name;
             $param_lastname = $last_name;
+            $param_university = 1;
             $param_type = 'volunteer';
             
+            //Insert into volunteers table now 
+            $result= mysqli_query($link,"SELECT MAX(userID) AS maximum FROM users");
+            $row = mysqli_fetch_array($result); 
+            $param_userID = $row['maximum'];
+
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt,'sssss', $param_email, $param_password, $param_firstname, $param_lastname, $param_type);
+            // mysqli_stmt_bind_param($stmt_university,'s', $param_university);
+            // mysqli_stmt_bind_param($stmt_volunteer,'ss', $param_userID, $param_university);
+
             // Attempt to execute the prepared statement
             if (mysqli_stmt_execute($stmt)) {
+            // if (mysqli_stmt_execute($stmt) && mysqli_stmt_execute($stmt_university) && mysqli_stmt_execute($stmt_volunteer)) {
                 // Redirect to home page
                 echo "You have successfully created a VDASH account!";
                 header("location: index.php");
-            } else {
-                echo 'You have successfully created a VDASH account!';
-            }
-            // Close statement
-            mysqli_stmt_close($stmt);
+            } 
         }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
     }
-    // Close connection
-    mysqli_close($link);
+
+    // Close statement
+    mysqli_stmt_close($stmt);
+}
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -116,13 +144,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <link rel="stylesheet"
 	href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <style>
-h2 {
+
+h3 {
     text-align: center;
     font-size: 25px; 
     padding-top: 25px; 
     font-family: "Verdana";
     font-weight: bold; 
 }
+
 p {
     text-align: center;
     font-size: 13px;
@@ -147,9 +177,11 @@ ul {
   background-color: #333;
 /*   float: right; */
 }
+
 li {
   float: left;
 }
+
 li a {
   display: block;
   color: white;
@@ -160,6 +192,7 @@ li a {
   padding-right: 20px; 
   text-decoration: none;
 }
+
 li a:hover {
     background-color: #111;
 }
@@ -167,14 +200,16 @@ li a:hover {
 </style>
 </head>
 <body>
+
 	<ul>
     	<li><a href="index.php" class="pull-left" style="height: auto"><img src="VDASH.png" style="height: 28px"></a><li>
-    	<li><a href="volunteer_v.php">Volunteer Portal</a></li>
+    	<li><a href="user_v.php">Volunteer Portal</a></li>
     	<li><a href="manager_v.php">Manager Portal</a></li>
     	<li class="active"><a href="register.php">Register</a></li>
 	</ul>
+
 	<div class="wrapper">
-		<h2>Sign Up</h2>
+		<h3>Sign Up</h3>
 		<p>Create a VDASH volunteer account by completing this form:</p>
         <div class="container mt-3 mb-3">
 			<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"
@@ -216,8 +251,8 @@ li a:hover {
 								class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
 						</div>
 						<div class="form-group">
-							<input type="submit" class="btn btn-primary" value="Submit"> <input
-								type="reset" class="btn btn-secondary ml-2" value="Reset">
+							<input type="submit" class="btn btn-primary" value="Submit"> 
+                            <input type="reset" class="btn btn-secondary ml-2" value="Reset">
 						</div>
 						<p>
 							Already have an account? <a href="login.php">Login here</a>.
